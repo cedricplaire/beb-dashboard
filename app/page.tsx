8231b99a0b16225c97f936c2c6fdd6c41c0f230a
Plaire Cédric <cedricplaire30@gmail.com>
2025-09-15 02:11:44 +0200

    Pre render and skeletons

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={Style.shape}>
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo open={true} />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
