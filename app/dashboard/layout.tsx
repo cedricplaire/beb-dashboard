09e89cb43f538a850e364ae07005e1296eeb6c2f
Plaire Cédric <cedricplaire30@gmail.com>
2025-09-15 03:34:10 +0200

    Create Edit Invoices

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideBar from "../ui/dashboard/sidebar";
 
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
